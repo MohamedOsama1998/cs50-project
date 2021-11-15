37ae6d5e298d86040fc0e92e20efca42793e83ec
Mohamed Osama <eng-mohamed.osama1621@alexu.edu.eg>
2021-11-15 07:27:32 +0200

    setting up SQL

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
 
 app = Flask(__name__)
+DATABASE = './database.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 if __name__ == "__main__":
