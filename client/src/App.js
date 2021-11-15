a839ca856ed17e9b2d8f0306be705b34c8471563
Mohamed Osama <eng-mohamed.osama1621@alexu.edu.eg>
2021-11-15 07:42:08 +0200

    setting up pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Land from "./pages/Land";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
     <div>
-      <h1>CS50 Project</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Land />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ client/src/pages/Land.js
@@ -0,0 +1,9 @@
+const Land = () => {
+  return (
+    <div>
+      <h1>Land here</h1>
+    </div>
+  );
+};
+
+export default Land;
