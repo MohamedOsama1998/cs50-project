b6e566308696b92794f49f989584f184df904dfc
Mohamed Osama <eng-mohamed.osama1621@alexu.edu.eg>
2021-11-16 10:08:16 +0200

    FRONTEND: login/register forms validations

--- client/src/forms/LoginForm.js
+++ client/src/forms/LoginForm.js
@@ -1,49 +1,80 @@
-import { useState } from "react";
-import LoadingButton from "@mui/lab/LoadingButton";
 import { TextField } from "@mui/material";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import LoadingButton from "@mui/lab/LoadingButton";
 
-const RegisterForm = () => {
-  const [isLoading, setLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const LoginForm = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Please enter a valid email address")
+      .required("Please enter your email"),
+    password: Yup.string()
+      .required("Please enter your password")
+      .min(8, "Your password must be at least 8 characters long"),
+  });
+
+  const onSubmit = (values, actions) => {
+    console.log(values);
+
+    actions.setSubmitting(false);
+  };
 
   return (
-    <form>
-      <TextField
-        autoComplete="new-password"
-        margin="normal"
-        variant="standard"
-        label="Email"
-        type="email"
-        required
-        fullWidth
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <TextField
-        autoComplete="new-password"
-        margin="normal"
-        variant="standard"
-        label="Password"
-        type="password"
-        required
-        fullWidth
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <LoadingButton
-        type="submit"
-        loading={isLoading}
-        variant="outlined"
-        size="large"
-        style={{
-          marginTop: "20px",
-        }}
-      >
-        Login
-      </LoadingButton>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ errors, isSubmitting, touched, values }) => (
+        <Form>
+          <Field
+            disabled={isSubmitting}
+            as={TextField}
+            autoComplete="off"
+            margin="normal"
+            variant="standard"
+            label="Email"
+            type="email"
+            name="email"
+            required
+            fullWidth
+            value={values.email}
+            error={errors.email && touched.email ? true : false}
+            helperText={<ErrorMessage name="email" />}
+          />
+          <Field
+            disabled={isSubmitting}
+            as={TextField}
+            margin="normal"
+            variant="standard"
+            label="Password"
+            type="password"
+            name="password"
+            required
+            fullWidth
+            value={values.password}
+            error={errors.password && touched.password ? true : false}
+            helperText={<ErrorMessage name="password" />}
+          />
+          <LoadingButton
+            type="submit"
+            loading={isSubmitting}
+            variant="outlined"
+            size="large"
+            style={{
+              marginTop: "20px",
+            }}
+          >
+            Login
+          </LoadingButton>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
-export default RegisterForm;
+export default LoginForm;
--- client/src/forms/RegisterForm.js
+++ client/src/forms/RegisterForm.js
@@ -1,72 +1,114 @@
-import { useState } from "react";
 import { TextField } from "@mui/material";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import LoadingButton from "@mui/lab/LoadingButton";
 
 const RegisterForm = () => {
-  const [isLoading, setLoading] = useState(false);
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confPassword, setConfPassword] = useState("");
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+    confPassword: "",
+  };
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required("Please enter a username"),
+    email: Yup.string()
+      .email("Please enter a valid email address")
+      .required("Please enter your email"),
+    password: Yup.string()
+      .required("Please enter your password")
+      .min(8, "Your password must be at least 8 characters long"),
+    confPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Please confirm your password"),
+  });
+
+  const onSubmit = (values, actions) => {
+    console.log(values);
+
+    actions.setSubmitting(false);
+  };
 
   return (
-    <form>
-      <TextField
-        autoComplete="new-password"
-        margin="normal"
-        variant="standard"
-        label="Username"
-        type="text"
-        required
-        fullWidth
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      ></TextField>
-      <TextField
-        autoComplete="new-password"
-        margin="normal"
-        variant="standard"
-        label="Email"
-        type="email"
-        required
-        fullWidth
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      ></TextField>
-      <TextField
-        autoComplete="new-password"
-        margin="normal"
-        variant="standard"
-        label="Password"
-        type="password"
-        required
-        fullWidth
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      ></TextField>
-      <TextField
-        autoComplete="new-password"
-        margin="normal"
-        variant="standard"
-        label="Confirm password"
-        type="password"
-        required
-        fullWidth
-        value={confPassword}
-        onChange={(e) => setConfPassword(e.target.value)}
-      ></TextField>
-      <LoadingButton
-        type="submit"
-        loading={isLoading}
-        variant="outlined"
-        size="large"
-        style={{
-          marginTop: "20px",
-        }}
-      >
-        Register
-      </LoadingButton>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ errors, isSubmitting, touched, values }) => (
+        <Form>
+          <Field
+            disabled={isSubmitting}
+            as={TextField}
+            autoComplete="off"
+            margin="normal"
+            variant="standard"
+            label="Username"
+            type="text"
+            name="username"
+            required
+            fullWidth
+            value={values.username}
+            helperText={<ErrorMessage name="username" />}
+            error={errors.username && touched.username ? true : false}
+          ></Field>
+          <Field
+            disabled={isSubmitting}
+            as={TextField}
+            autoComplete="off"
+            margin="normal"
+            variant="standard"
+            label="Email"
+            type="email"
+            name="email"
+            required
+            fullWidth
+            value={values.email}
+            error={errors.email && touched.email ? true : false}
+            helperText={<ErrorMessage name="email" />}
+          />
+          <Field
+            disabled={isSubmitting}
+            as={TextField}
+            margin="normal"
+            variant="standard"
+            label="Password"
+            type="password"
+            name="password"
+            required
+            fullWidth
+            value={values.password}
+            error={errors.password && touched.password ? true : false}
+            helperText={<ErrorMessage name="password" />}
+          />
+          <Field
+            disabled={isSubmitting}
+            as={TextField}
+            variant="standard"
+            margin="normal"
+            label="Confirm password"
+            type="password"
+            name="confPassword"
+            required
+            fullWidth
+            value={values.confPassword}
+            error={errors.confPassword && touched.confPassword ? true : false}
+            helperText={<ErrorMessage name="confPassword" />}
+          />
+          <LoadingButton
+            type="submit"
+            loading={isSubmitting}
+            variant="outlined"
+            size="large"
+            style={{
+              marginTop: "20px",
+            }}
+          >
+            Register
+          </LoadingButton>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
